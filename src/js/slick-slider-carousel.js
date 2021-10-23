d6c8c9ec4c2d8b1e2404c219454caf67203049ee
WebGertsog <groxenvil@gmail.com>
2021-10-23 13:51:16 +0300

    Add more images of products to the order page and add carousel
    1. Fix the error in the jQuery library:
    1.1 Delete unnecessary manifest.json file (... src / images / favicon);
    1.2 Remove connection of the manifest.json file on all pages of the site;
    1.3 Remove unused images (... src / images / desktop / take-order).
    2. Load and optimize 3 images from the "More photos" gallery on the main page
    3. Create additional markup on the take-order page and write the path to new images and add a description.
    4. Connect the carousel slider:
    4.1 Include jQuery library on the "take-order" page;
    4.2 Include the "slick slider" library on the "take-order" page;
    4.3 Install the "slick slider" library using "npm";
    4.4 Create settings file "slick-slider-carousel.js"
    4.5 Include the settings file "slick-slider-carousel.js" on the "take-order" page.

---
+++ src/js/slick-slider-carousel.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+  arrows: false,
+  infinite:	true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+});
