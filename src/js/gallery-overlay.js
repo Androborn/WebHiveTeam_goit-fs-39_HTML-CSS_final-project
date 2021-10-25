5f224c9df2207b31e9ba27322003da761bc6a5f1
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2021-10-25 16:05:50 +0300

    [refactor] Refactor .is-hidden base style
    Styles:
    
    - Refactor .is-hidden base style to --is-hidden modificator in mobile menu and main page more photo section gallery overlay

--- src/js/gallery-overlay.js
+++ src/js/gallery-overlay.js
@@ -12,6 +12,6 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('--is-hidden');
   }
 })();
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,6 @@
 (() => {
   const refs = {
-        body: document.querySelector('body'),
+    body: document.querySelector('body'),
     header: document.querySelector('.header'),
     openModalBtn: document.querySelector('[mobile-menu-open]'),
     modal: document.querySelector('[mobile-menu]'),
@@ -9,8 +9,8 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-        refs.body.classList.toggle('mobile-menu-is-open')
-    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('mobile-menu-is-open');
+    refs.modal.classList.toggle('--is-hidden');
     refs.header.classList.toggle('header--mobile-menu-is-open');
     refs.openModalBtn.classList.toggle('hamburger--collapse');
   }
