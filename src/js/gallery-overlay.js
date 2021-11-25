da1e4ad2825f629e80fde8b8d1cb87f84756d223
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2021-11-25 14:55:46 +0200

    [feat] Add library Simplelightbox to main page
    HTML-markup:
    - Delete gallery-overlay.html
    - Remove atribute gallery-overlay-open from li-elements
    - add a-tags to li-elements
    - Add script to main-page.html
    
    JS:
    - Remove unnecessary script from gallery-overlay.js
    - Add SipleLightbox at gallery-overlay.js

--- src/js/gallery-overlay.js
+++ src/js/gallery-overlay.js
@@ -1,19 +1,4 @@
-(() => {
-  const refs = {
-    body: document.querySelector('body'),
-    openModalBtns: document.querySelectorAll(
-      '[gallery-overlay-open]',
-    ),
-    closeModalBtn: document.querySelector('[gallery-overlay-close]'),
-    modal: document.querySelector('[gallery-overlay]'),
-  };
-  for (const el of refs.openModalBtns) {
-    el.addEventListener('click', toggleModal);
-  }
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('gallery-overlay-is-open');
-  }
-})();
+const lightbox = new SimpleLightbox('.more-photo__list a');
